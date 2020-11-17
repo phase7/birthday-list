6a80f58088524a8977b7fd6713702060ae5f4230
phase seven <ratulroy20@gmail.com>
2020-11-17 13:50:19 +0600

    a working project is done needs styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import React, {useState} from 'react';
-import {List} from './components/ListComponent'
+import React, { FormEvent, useState } from 'react';
+import { List } from './components/ListComponent'
 //data
 import Data from "./shared/data"
+//types
+import { Entry } from "./shared/data";
 //style
 import './App.css';
 
 
 function App() {
+  const [people, setPeople] = useState<Entry[]>(Data);
+
+  function handleSubmit(event: FormEvent<HTMLButtonElement>): void {setPeople([]);
+  }
   return (
     <div className="App">
-      
+      <div className="container">
+        <h3 className="birthday-status">{people.length} people have birthday today!</h3>
+        <List people={Data}></List>
+        <button onSubmit={handleSubmit}></button>
+      </div>
     </div>
   );
 }
